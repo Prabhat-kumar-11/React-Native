35f0e55a463aeae0197af5b1d6dd332796709efe
Prabhat Kumar <prabhssgg3@gmail.com>
2023-10-18 16:47:43 +0530

    start with basics

---
+++ App.tsx
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    // <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>hello world</Text>
+      </View>
+    // </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "red",
+  },
+  text: {
+    color: 'green',
+  },
+});
+
+export default App;
